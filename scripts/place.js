c05173563e39bd63a49b36b6d2ccb9f4604d932b
Timilehin Daramola <timi.homezone111@gmail.com>
2024-05-08 19:58:10 -0400

    wdd131

---
+++ scripts/place.js
@@ -0,0 +1,14 @@
+//select the DOM element.
+const year = document.querySelector("#currentyear");
+let lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+lastModified.innerHTML = `<span class="highlight">Last Modification: ${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short"
+	}
+).format(today)}</span>`;
