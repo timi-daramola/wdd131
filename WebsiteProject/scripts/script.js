222729b4de90a69c9c7187ecd451d1054b5790b2
Timilehin Daramola <timi.homezone111@gmail.com>
2024-06-01 01:45:33 -0400

    wdd131

---
+++ WebsiteProject/scripts/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Lazy load images
+    const images = document.querySelectorAll('img[data-src]');
+    images.forEach(img => {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.onload = function() {
+            img.removeAttribute('data-src');
+        };
+    });
+});
+
+
+const year = document.querySelector("#currentyear");
+let lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+lastModified.innerHTML = `<span class="highlight">Last Modification: ${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short"
+	}
+).format(today)}</span>`;
